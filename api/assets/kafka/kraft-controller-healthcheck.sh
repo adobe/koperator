19f7e8d16597511463e13bfe01e45b54f6a1f923
Ha Van <hvan@redhat.com>
2025-08-12 14:35:47 -0500

    Add license to script

--- api/assets/kafka/kraft-controller-healthcheck.sh
+++ api/assets/kafka/kraft-controller-healthcheck.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2025 Cisco Systems, Inc. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # This script returns a successful exit code (0) if the controller is a follower or leader.  For any other state, it returns a failure exit code (1).
 
