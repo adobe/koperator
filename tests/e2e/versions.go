797a4b0d8e75dc814c07472ac8145dc000bbe490
Adi Muraru <amuraru@adobe.com>
2026-01-07 12:50:14 +0100

    chore(deps): update all non-major dependencies

--- tests/e2e/versions.go
+++ tests/e2e/versions.go
@@ -19,13 +19,13 @@ package e2e
 // These versions are monitored by Renovate for automatic updates
 const (
 	// CertManagerVersion is the version of cert-manager Helm chart
-	CertManagerVersion = "v1.19.1" // renovate: datasource=helm depName=cert-manager registryUrl=https://charts.jetstack.io
+	CertManagerVersion = "v1.19.2" // renovate: datasource=helm depName=cert-manager registryUrl=https://charts.jetstack.io
 
 	// ContourVersion is the version of Contour ingress controller Helm chart
-	ContourVersion = "0.2.0" // renovate: datasource=helm depName=contour registryUrl=https://projectcontour.github.io/helm-charts
+	ContourVersion = "0.2.1" // renovate: datasource=helm depName=contour registryUrl=https://projectcontour.github.io/helm-charts
 
 	// PrometheusOperatorVersion is the version of kube-prometheus-stack Helm chart
-	PrometheusOperatorVersion = "80.4.1" // renovate: datasource=helm depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
+	PrometheusOperatorVersion = "80.13.0" // renovate: datasource=helm depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
 
 	// ZookeeperOperatorVersion is the version of zookeeper-operator
 	ZookeeperOperatorVersion = "0.2.15-adobe-20250923" // renovate: datasource=docker depName=ghcr.io/adobe/helm-charts/zookeeper-operator
